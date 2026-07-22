bfd28bdc4f0dc4a400d360cd54bb4dfe559978bd
anarchysoundllc <anarchysoundllc@gmail.com>
2026-07-22 10:37:30 -0400

    removed extra 0 in LED config

--- keyboards/handwired/anarchy_qpad/anarchy_qpad.c
+++ keyboards/handwired/anarchy_qpad/anarchy_qpad.c
@@ -3,12 +3,12 @@
 #ifdef LED_MATRIX_ENABLE
 const is31fl3218_led_t PROGMEM g_is31fl3218_leds[IS31FL3218_LED_COUNT] = {
 /*   V */
-    {0, OUT1}, //KC_1
-    {0, OUT2}, //KC_2
-    {0, OUT3}, //KC_3
-    {0, OUT4}, //KC_4
-    {0, OUT5}, //KC_5
-    {0, OUT6}  //KC_6
+    {OUT1}, //KC_1
+    {OUT2}, //KC_2
+    {OUT3}, //KC_3
+    {OUT4}, //KC_4
+    {OUT5}, //KC_5
+    {OUT6}  //KC_6
     // etc...
 };
 
@@ -27,9 +27,3 @@ led_config_t g_led_config = { {
   0, 0
 } };
 #endif
-#define OUT1 0x00
-#define OUT2 0x01
-#define OUT3 0x02
-#define OUT4 0x03
-#define OUT5 0x04
-#define OUT6 0x05
