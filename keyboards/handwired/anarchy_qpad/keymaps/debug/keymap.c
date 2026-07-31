48265e88061d3c2999308433209ec17e35e7c534
anarchysoundllc <anarchysoundllc@gmail.com>
2026-07-31 13:55:35 -0400

    Removed Breathing
    Removed code related to breathing since that never worked.

--- keyboards/handwired/anarchy_qpad/keymaps/debug/keymap.c
+++ keyboards/handwired/anarchy_qpad/keymaps/debug/keymap.c
@@ -15,8 +15,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT(
         BL_TOGG,   BL_BRTG,
-        BL_STEP,   BL_ON,
-        BL_UP,   BL_DOWN
+        LM_PREV,   LM_NEXT,
+        LM_BRID,   LM_BRIU
     )
 };
 void keyboard_post_init_user(void) {
